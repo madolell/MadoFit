63c0d034291a9f92bf2fa1e7de9263658e7ab8ba
madolell <100812732+madolell@users.noreply.github.com>
2023-05-22 23:58:59 +0200

    Add files via upload

--- Entrenamiento/Ejercicios.php
+++ Entrenamiento/Ejercicios.php
@@ -98,7 +98,7 @@ include("../Auxiliares/conecta.php");
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-2 g-4 d-flex align-item-center">
             <div class="col">
                 <div class="card position-relative shadow-sm border-0">
-                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=pectorales"><img src="../fotos/Grupo Muscular/pecho.jpg" class="card-img-top"
+                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=Pectorales"><img src="../fotos/Grupo Muscular/pecho.jpg" class="card-img-top"
                             alt="Pectorales">
                         <div class="card-body position-absolute bottom-0 start-0 text-light">
                             <h6 class="card-title">Pectorales</h6>
@@ -108,7 +108,7 @@ include("../Auxiliares/conecta.php");
             </div>
             <div class="col">
                 <div class="card position-relative shadow-sm border-0">
-                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=espalda"><img src="../fotos/Grupo Muscular/espalda.jpg" class="card-img-top"
+                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=Espalda"><img src="../fotos/Grupo Muscular/espalda.jpg" class="card-img-top"
                             alt="Espalda">
                         <div class="card-body position-absolute bottom-0 start-0 text-light">
                             <h6 class="card-title">Espalda</h6>
@@ -118,7 +118,7 @@ include("../Auxiliares/conecta.php");
             </div>
             <div class="col">
                 <div class="card position-relative shadow-sm border-0">
-                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=piernas"><img src="../fotos/Grupo Muscular/pierna.jpg" class="card-img-top"
+                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=Piernas"><img src="../fotos/Grupo Muscular/pierna.jpg" class="card-img-top"
                             alt="Pierna">
                         <div class="card-body position-absolute bottom-0 start-0 text-light">
                             <h6 class="card-title">Piernas</h6>
@@ -128,7 +128,7 @@ include("../Auxiliares/conecta.php");
             </div>
             <div class="col">
                 <div class="card position-relative shadow-sm border-0">
-                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=hombros"><img src="../fotos/Grupo Muscular/hombro.jpg" class="card-img-top"
+                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=Hombros"><img src="../fotos/Grupo Muscular/hombro.jpg" class="card-img-top"
                             alt="Hombros">
                         <div class="card-body position-absolute bottom-0 start-0 text-light">
                             <h6 class="card-title">Hombros</h6>
@@ -138,7 +138,7 @@ include("../Auxiliares/conecta.php");
             </div>
             <div class="col">
                 <div class="card position-relative shadow-sm border-0">
-                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=biceps"><img src="../fotos/Grupo Muscular/biceps.jpg" class="card-img-top"
+                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=Biceps"><img src="../fotos/Grupo Muscular/biceps.jpg" class="card-img-top"
                             alt="Bíceps">
                         <div class="card-body position-absolute bottom-0 start-0 text-light">
                             <h6 class="card-title">Bíceps</h6>
@@ -148,7 +148,7 @@ include("../Auxiliares/conecta.php");
             </div>
             <div class="col">
                 <div class="card position-relative shadow-sm border-0">
-                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=triceps"><img src="../fotos/Grupo Muscular/triceps.jpg" class="card-img-top"
+                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=Triceps"><img src="../fotos/Grupo Muscular/triceps.jpg" class="card-img-top"
                             alt="Tríceps">
                         <div class="card-body position-absolute bottom-0 start-0 text-light">
                             <h6 class="card-title">Tríceps</h6>
@@ -158,7 +158,7 @@ include("../Auxiliares/conecta.php");
             </div>
             <div class="col">
                 <div class="card position-relative shadow-sm border-0">
-                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=abdominales"><img src="../fotos/Grupo Muscular/abdominales.jpg" class="card-img-top"
+                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=Abdominales"><img src="../fotos/Grupo Muscular/abdominales.jpg" class="card-img-top"
                             alt="Abdominales">
                         <div class="card-body position-absolute bottom-0 start-0 text-light">
                             <h6 class="card-title">Abdominales</h6>
@@ -168,7 +168,7 @@ include("../Auxiliares/conecta.php");
             </div>
             <div class="col">
                 <div class="card position-relative shadow-sm border-0">
-                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=gemelos"><img src="../fotos/Grupo Muscular/gemelos.jpg" class="card-img-top"
+                    <a href="Ejercicios/Grupo_muscular.php?grupoMuscular=Gemelos"><img src="../fotos/Grupo Muscular/gemelos.jpg" class="card-img-top"
                             alt="Gemelos">
                         <div class="card-body position-absolute bottom-0 start-0 text-light">
                             <h6 class="card-title">Gemelos</h6>
