db0ebfe14fcd2d5688004f2ca9d00c4bb69e951c
madolell <100812732+madolell@users.noreply.github.com>
2023-05-22 23:49:47 +0200

    Add files via upload

--- Entrenamiento/Ejercicios/Grupo_muscular.php
+++ Entrenamiento/Ejercicios/Grupo_muscular.php
@@ -134,11 +134,7 @@ $mysqli->close();
             <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z" />
           </svg></a></li>
       <li class="breadcrumb-item"><a href="../../Entrenamiento.php" class="text-decoration-none text-dark">Entrenamiento</a></li>
-      <?php if (!empty($nombreGrupoMuscular)) { ?>
-            <li class="breadcrumb-item active" aria-current="page"><?php echo $nombreGrupoMuscular; ?></li>
-        <?php } else { ?>
-            <li class="breadcrumb-item active" aria-current="page">Grupo Muscular</li>
-        <?php } ?>
+      <li class="breadcrumb-item active" aria-current="page"><?php echo isset($nombreGrupoMuscular) ? $nombreGrupoMuscular : "Grupo Muscular"; ?></li>
     </ol>
   </div>
   <!-- Migas de pan -->
